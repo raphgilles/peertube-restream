19d29cf90811e3e7297ff4eb8750b1be740ebdda
Raph <49836486+raphgilles@users.noreply.github.com>
2022-12-28 17:11:52 +0100

    Add files via upload

--- youtube/index.php
+++ youtube/index.php
@@ -37,9 +37,7 @@ paceOptions = {
 
 <body>
 
-<div class="header">
-  <p><a href="https://tooter.social/@raph" id="headerlink">Mastodon</a> <a href="https://peertube.stream/accounts/raph" id="headerlink">PeerTube</a></p>
-</div>
+<?php include __DIR__."/../header.php" ?>
 
 <?php exec("pgrep ffmpeg", $pids);
 if(empty($pids)) { ?>
@@ -50,7 +48,7 @@ if(empty($pids)) { ?>
 
 <form action="golive.php" method="post" enctype="multipart/form-data">
 
-<h3 style="text-align:left; font-family:robotolight; margin-top:75px;">URL de l'instance (sans https:// et sans le / à la fin)</h3>
+<h3 style="text-align:left; font-family:robotolight; margin-top:75px;">URL de l'instance PeerTube</h3>
 <textarea name="instance" class="inputvideo" required></textarea>
 
 <h3 style="text-align:left; font-family:robotolight; margin-top:25px;">Clé de diffusion du direct</h3>
@@ -93,8 +91,7 @@ Pour me soutenir financièrement :<br />
 
 <?php } ?>
 
-<div class="footer">
-  <p><img src="../images/peertube.png" style="height:25px; vertical-align:-7px; margin-right:5px;" /> <a href="https://joinpeertube.org/instances" id="footerlink">Trouver une instance PeerTube</a></p>
-</div>
+<?php include __DIR__."/../footer.php" ?>
+
 </body>
 </html>
